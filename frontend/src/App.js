6d5530923db3f14cbe5750c11592eabb2ffe8328
vickydarlinn <uttapalsangwan@gmail.com>
2023-08-24 16:55:21 +0530

    fix:fixed the server url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client";
 
-const socket = socketIO.connect("http://localhost:5656");
+const socket = socketIO.connect("https://server-g26-app.onrender.com");
 function App() {
   return (
     <BrowserRouter>
